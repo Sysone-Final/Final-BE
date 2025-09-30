bcd685c70762c427c07ad5245dfacfa611a8a2d1
snikuz12 <snikuz12@naver.com>
2025-09-30 15:33:42 +0900

    Initial commit

---
+++ src/test/java/org/example/finalbe/FinalBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.finalbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
